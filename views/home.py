8e2fa6b16912f58e77647a0fb156fa074e1577fa
Jhonatan Pablo Llaja Julca <i2512338@continental.edu.pe>
2025-08-06 16:36:12 -0500

    creando home

---
+++ views/home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def mostrar():
+    st.title("Bienvenido al simulador bancario")
+    st.markdown("""
+                
+            Esta aplicacion permite simular creditos hipotecarios y vehiculares.
+            Proximamente se conectara con Supabase para guardar las simulaciobes
+            """
+                )
