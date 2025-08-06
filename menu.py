8a2d8f4cc9e980dcf6b306e374419d7648ba8dae
Jhonatan Pablo Llaja Julca <i2512338@continental.edu.pe>
2025-08-06 18:21:56 -0500

    modificando simulador

--- menu.py
+++ menu.py
@@ -4,7 +4,8 @@ from views import home, simulador, reportes
 def mostrar_menu():
     opciones = {
         "Inicio":home.mostrar,
-        
+        "Simulador": simulador.mostrar,
+        "Reportes": reportes.mostrar
     }
     
     seleccion = st.sidebar.selectbox("Navegacion", list(opciones.keys()))
