b20bc93172c421178abb9d485193eb7b6a168ff2
Jhonatan Pablo Llaja Julca <i2512338@continental.edu.pe>
2025-08-06 18:09:58 -0500

    modificando simulador

--- models/credit.py
+++ models/credit.py
@@ -4,13 +4,12 @@ class CreditoHipotecario(BaseCredito):
     def calcular_cuota(self):
         r = self.tasa / 12 / 100
         n = self.plazo * 12
-        cuota = self.monto * (r * (1+r)**n)/((1+r)**n -1)
+        cuota = self.monto * (r * (1 + r) ** n) / ((1 + r) ** n - 1)
         return round(cuota, 2)
-    
-    class CreditoVehicular(BaseCredito):
-        def calcular_cuota(self):
-            r = self.tasa / 12 / 100
-             n = self.plazo * 12
-             cuota = self.monto * (r * (1+r)**n)/((1+r)**n -1)
-            return round(cuota * 1.05, 2) #incluye seguro vehicular
-        
+
+class CreditoVehicular(BaseCredito):
+    def calcular_cuota(self):
+        r = self.tasa / 12 / 100
+        n = self.plazo * 12
+        cuota = self.monto * (r * (1 + r) ** n) / ((1 + r) ** n - 1)
+        return round(cuota * 1.05, 2)  # incluye seguro vehicular
